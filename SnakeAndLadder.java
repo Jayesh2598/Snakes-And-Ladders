51debdf698f64b269cd2878377dd815bb4a72ebf
Jay007 <jayesh2598@gmail.com>
2020-09-24 18:06:42 +0530

    Created: Snake and Ladder game is initialized for single player

---
+++ SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.capgemini.training;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("***** Snake and Ladder Game *****");
+		
+		int playerScore=0;
+		System.out.println("The initial player score is set to "+ playerScore);
+
+	}
+
+}

84b62d6ec8da2eb53868c78a8486ea671f1eb484
Jay007 <jayesh2598@gmail.com>
2020-09-24 19:04:14 +0530

    Added: Feature selecting option of Snake/Ladder/No Play is added and final player position is displayed

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,10 +8,30 @@ public class SnakeAndLadder {
 		
 		int playerScore=0;
 		System.out.println("The initial player score is set to "+ playerScore);
-
+		
+		//Rolling the die
 		System.out.println("Rolling a die... ");
 		int score = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-		System.out.println("Score generated is :" + score);
+		System.out.println("Score generated:" + score);
+		
+		//Selecting the option
+		int option = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (option) {
+			case 1:
+				System.out.println("Option: Ladder");
+				playerScore += score;
+				break;
+			case 2:
+				System.out.println("Option: Snake");
+				playerScore -= score;
+				if(playerScore<0)
+					playerScore=0;
+				break;
+			default:
+				System.out.println("Option: No Play");
+		}
+		
+		System.out.println("The final player position is :"+ playerScore);
 		
 	}
 

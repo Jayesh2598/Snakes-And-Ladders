930ff8746334ceb3e35432b147b8978848a58c00
Jay007 <jayesh2598@gmail.com>
2020-09-24 20:55:53 +0530

    Added: While loop for repetition till player reaches >=100 position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,37 +1,41 @@
 package com.capgemini.training;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+		
+		Random ran=new Random();
 		
 		System.out.println("***** Snake and Ladder Game *****");
 		
 		int playerScore=0;
 		System.out.println("The initial player score is set to "+ playerScore);
 		
-		//Rolling the die
-		System.out.println("Rolling a die... ");
-		int score = (int) (Math.floor(Math.random() * 10) % 6) + 1;
-		System.out.println("Score generated:" + score);
+		while(playerScore<100) {
+			//Rolling the die
+			int score = ran.nextInt(6) + 1;;
+			
+			//Selecting the option
+			int option = ran.nextInt(3);
+			
+			//Choosing action from option
+			switch (option) {
+				case 1:
+					playerScore += score;
+					break;
+				case 2:
+					playerScore -= score;
+					if(playerScore<0)
+						playerScore=0;
+					break;
+				default:
+					break;
+			}
+		}	
 		
-		//Selecting the option
-		int option = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (option) {
-			case 1:
-				System.out.println("Option: Ladder");
-				playerScore += score;
-				break;
-			case 2:
-				System.out.println("Option: Snake");
-				playerScore -= score;
-				if(playerScore<0)
-					playerScore=0;
-				break;
-			default:
-				System.out.println("Option: No Play");
-		}
-		
-		System.out.println("The final player position is :"+ playerScore);
+		System.out.println("The player has won after reaching "+ playerScore+ " position");
 		
 	}
 

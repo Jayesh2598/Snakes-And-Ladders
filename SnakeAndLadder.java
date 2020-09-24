587616d463c0c9e78c1a3f859ba26eac33d8da08
Jay007 <jayesh2598@gmail.com>
2020-09-24 18:20:58 +0530

    Added: Die rolling feature added and random score generated

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,6 +8,10 @@ public class SnakeAndLadder {
 		
 		int playerScore=0;
 		System.out.println("The initial player score is set to "+ playerScore);
+
+		System.out.println("Rolling a die... ");
+		int score = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+		System.out.println("Score generated is :" + score);
 		
 	}
 
